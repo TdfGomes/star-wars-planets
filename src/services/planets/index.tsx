f24d232f195ebd57124795f04666fc665442af42
TdfGomes <t.dfgomes@icloud.com>
2024-11-10 18:33:10 +0000

    feat: get planet by id service

--- src/services/planets/index.tsx
+++ src/services/planets/index.tsx
@@ -1,4 +1,4 @@
-import { PlanetsResult } from "@/types";
+import { Planet, PlanetsResult } from "@/types";
 import { BASE_URL } from "../constants";
 
 export async function getPlanets(search: string | null, page: string): Promise<PlanetsResult> {
@@ -20,5 +20,21 @@ export async function getPlanets(search: string | null, page: string): Promise<P
     console.error(e);
     //throw an useful error to the user
     throw new Error("Something went wrong. It's not possible to fetch planets at the moment");
+  }
+}
+
+export async function getPlanetById(id: string): Promise<Planet> {
+  try {
+    const data = await fetch(`${BASE_URL}/planets/${id}`);
+
+    return data.json();
+  } catch (e) {
+    //send the error to a monitoring tool
+    //sendError(e.message)
+    console.error(e);
+    //throw an useful error to the user
+    throw new Error(
+      `Something went wrong. It's not possible to fetch planet with id: ${id} at the moment`,
+    );
   }
 }
